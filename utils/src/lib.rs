6bd912e5af4e7ba30ca6470594f464a0f2f936a3
peter <peter.keyzers@gmail.com>
2018-12-18 11:54:16 +0000

    Add read_file util function.

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,11 +1,19 @@
 use std::{
     io,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, Read},
     fs::File,
 };
 
 
-pub fn lines_from_file(filename: String) -> io::Result<Vec<String>>
-{
+pub fn lines_from_file(filename: String) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
+}
+
+
+pub fn read_file(filename: String) -> String {
+    let mut output = String::new();
+
+    File::open(filename).expect("Unable to open file").read_to_string(&mut output).expect("Unable to read filename");
+
+    output
 }
